0693b3ddfed5b99ea1f2290de38ee1740d9cc5bb
lmj0011 <9396189+lmj0011@users.noreply.github.com>
2021-08-27 21:00:28 -0500

    - provide reddit auth alternative in case user can't login through webview

--- app/src/main/java/name/lmj0011/holdup/ui/redditauthwebview/RedditAuthWebviewFragment.kt
+++ app/src/main/java/name/lmj0011/holdup/ui/redditauthwebview/RedditAuthWebviewFragment.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.ConsoleMessage
@@ -13,6 +15,9 @@ import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -28,12 +33,13 @@ import name.lmj0011.holdup.helpers.factories.ViewModelFactory
 import name.lmj0011.holdup.helpers.util.launchIO
 import name.lmj0011.holdup.helpers.util.showSnackBar
 import name.lmj0011.holdup.BuildConfig
+import name.lmj0011.holdup.helpers.util.openUrlInWebBrowser
 import org.json.JSONObject
 import org.kodein.di.instance
 import timber.log.Timber
 
 class RedditAuthWebviewFragment : Fragment() {
-
+    private lateinit var rootView: View
     private lateinit var redditAuthHelper: RedditAuthHelper
     private lateinit var redditApiHelper: RedditApiHelper
     private val  viewModel by viewModels<RedditAuthWebviewViewModel> {
@@ -47,9 +53,9 @@ class RedditAuthWebviewFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_reddit_auth_webview, container, false)
-        val browser: WebView = root.findViewById(R.id.reddit_auth_webview)
+    ): View {
+        rootView = inflater.inflate(R.layout.fragment_reddit_auth_webview, container, false)
+        val browser: WebView = rootView.findViewById(R.id.reddit_auth_webview)
         var username = ""
         redditAuthHelper = (requireContext().applicationContext as App).kodein.instance()
         redditApiHelper = (requireContext().applicationContext as App).kodein.instance()
@@ -61,12 +67,12 @@ class RedditAuthWebviewFragment : Fragment() {
                     when(consoleMsg.messageLevel()) {
                         ConsoleMessage.MessageLevel.ERROR -> {
                             val msg = "webChromeClient JS error: ${consoleMsg.message()}"
-                            showSnackBar(root, msg)
+                            Timber.e(msg)
                         }
                         else -> {
                             if (BuildConfig.FLAVOR == "preview") {
                                 val msg = "webChromeClient console message: ${consoleMsg.message()}"
-                                showSnackBar(root, msg)
+                                Timber.d(msg)
                             }
 
                         }
@@ -81,42 +87,8 @@ class RedditAuthWebviewFragment : Fragment() {
                 if (redditAuthHelper.authClient().isRedirectedUrl(url)) {
                     browser.stopLoading()
 
-                    // We will retrieve the bearer on the background thread.
                     launchIO{
-                        val account = viewModel.createNewAccount(username)
-
-                        try {
-                          account?.let {
-                             redditAuthHelper.authClient(it).getTokenBearer(url)
-                              val res = redditApiHelper.get("user/${username.substring(2)}/about", redditAuthHelper.authClient(it).getSavedBearer().getAccessToken()!!)
-                              val json = JSONObject(res.body!!.source().readUtf8())
-
-                              try {
-                                  val iconImg = json.getJSONObject("data").getString("icon_img")
-                                  if (!iconImg.isNullOrBlank()) account.iconImage = iconImg.split("?")[0]
-
-                                  try {
-                                      val snooImg = json.getJSONObject("data").getString("snoovatar_img")
-                                      if (!snooImg.isNullOrBlank()) account.iconImage = snooImg.split("?")[0]
-                                  }
-                                  catch (ex: org.json.JSONException) {
-                                      showSnackBar(root, "No snoovatar found for ${account.name}")
-                                  }
-                              }
-                              catch (ex: org.json.JSONException) {
-                                  showSnackBar(root, "Unable to set icon_img on this Account: ${account.name}")
-                              }
-                              finally {
-                                  viewModel.updateAccount(account)
-                              }
-                          }
-                        } catch (ex: AccessDeniedException) {
-                            account?.let {
-                                redditAuthHelper.authClient(it).getSavedBearer().revokeToken()
-                                viewModel.deleteAccount(it)
-                            }
-                            showSnackBar(root, ex.message.toString())
-                        }
+                        getAccountTokenBearer(username, url)
 
                         withContext(Dispatchers.Main){
                             findNavController().navigateUp()
@@ -127,7 +99,14 @@ class RedditAuthWebviewFragment : Fragment() {
 
             override fun onPageFinished(view: WebView, url: String?) {
                 // try to grab the username to assign to an `Account`
-                val query = "document.querySelector('body > div.content > div > h1 > a').innerText;"
+                val query = """
+                    (function () {
+                        const ele = document.querySelector('body > div.content > div > h1 > a');
+                    
+                        if (ele !== null) { return ele.innerText; }
+                        else return null;
+                    })();
+                    """.trimIndent()
                 view.evaluateJavascript(query) { queryResult ->
                     Timber.d("queryResult: $queryResult")
                     if(queryResult != "null") username = queryResult.replaceFirst("\"", "u/").replace("\"", "")
@@ -141,8 +120,12 @@ class RedditAuthWebviewFragment : Fragment() {
             ) {
                 val msg = error?.description.toString()
 
-                if(msg.isNotBlank()) showSnackBar(root, msg)
-                else showSnackBar(root, "WebViewClient encountered an error, errorCode: ${error?.errorCode}")
+                Timber.e(msg)
+
+                if (BuildConfig.FLAVOR == "preview") {
+                    if(msg.isNotBlank()) showSnackBar(rootView, msg)
+                    else showSnackBar(rootView, "WebViewClient encountered an error, errorCode: ${error?.errorCode}")
+                }
 
                 super.onReceivedError(view, request, error)
             }
@@ -151,13 +134,88 @@ class RedditAuthWebviewFragment : Fragment() {
         browser.settings.javaScriptEnabled = true
         browser.loadUrl(redditAuthHelper.authClient().provideAuthorizeUrl())
 
-        return root
+        rootView.findViewById<Button>(R.id.buttonSubmit).setOnClickListener {
+            var user = rootView.findViewById<EditText>(R.id.editTextTextUsername).text.toString()
+            val url = rootView.findViewById<EditText>(R.id.editTextCallbackUrl).text.toString()
+
+            if(user.substring(0,2) != "u/") {
+                user = "u/$user"
+            }
+
+            rootView.findViewById<LinearLayout>(R.id.manualCredentialsInputContainer)
+                .visibility = View.GONE
+
+            launchIO{
+                getAccountTokenBearer(user, url)
+
+                withContext(Dispatchers.Main){
+                    findNavController().navigateUp()
+                }
+            }
+        }
+
+        return rootView
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        super.onPrepareOptionsMenu(menu)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.reddit_auth, menu)
+    }
 
-        menu.findItem(R.id.action_manage_accounts)?.isVisible = false
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_open_in_browser -> {
+                openUrlInWebBrowser(requireContext(), redditAuthHelper.authClient().provideAuthorizeUrl())
+                true
+            }
+            R.id.action_enter_callback_url -> {
+                rootView.findViewById<LinearLayout>(R.id.manualCredentialsInputContainer)
+                    .visibility = View.VISIBLE
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    suspend fun getAccountTokenBearer(username: String, callbackUrl: String) {
+        val account = viewModel.createNewAccount(username)
+
+        try {
+            account?.let {
+                redditAuthHelper.authClient(it).getTokenBearer(callbackUrl)
+                val res = redditApiHelper.get("user/${username.substring(2)}/about", redditAuthHelper.authClient(it).getSavedBearer().getAccessToken()!!)
+                val json = JSONObject(res.body!!.source().readUtf8())
+
+                try {
+                    val iconImg = json.getJSONObject("data").getString("icon_img")
+                    if (!iconImg.isNullOrBlank()) account.iconImage = iconImg.split("?")[0]
+
+                    try {
+                        val snooImg = json.getJSONObject("data").getString("snoovatar_img")
+                        if (!snooImg.isNullOrBlank()) account.iconImage = snooImg.split("?")[0]
+                    }
+                    catch (ex: org.json.JSONException) {
+                        showSnackBar(rootView, "No snoovatar found for ${account.name}")
+                    }
+                }
+                catch (ex: org.json.JSONException) {
+                    showSnackBar(rootView, "Unable to set icon_img on this Account: ${account.name}")
+                }
+                finally {
+                    viewModel.updateAccount(account)
+                }
+            }
+        } catch (ex: AccessDeniedException) {
+            account?.let {
+                redditAuthHelper.authClient(it).getSavedBearer().revokeToken()
+                viewModel.deleteAccount(it)
+            }
+            showSnackBar(rootView, ex.message.toString())
+        }
     }
 
 }
